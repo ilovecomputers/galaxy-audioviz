9bf26a55017e038767625bc5c6715ed74078b189
i❤computers <i.love.comput3rs@gmail.com>
2014-11-23 21:06:16 -0800

    Refactor JS code

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 var nx = 20,
-    ny = 20,
-    a = [],
-    b = [],
-    c = [],
-    sound,
-    fft,
-    i,
-    j;
+  ny = 20,
+  a = [],
+  b = [],
+  c = [],
+  sound,
+  fft,
+  i,
+  j;
 
-function preload(){
- sound = loadSound('assets/lemoncreme.wav');
+function preload() {
+  sound = loadSound('assets/lemoncreme.wav');
 }
 function setup() {
   createCanvas(1024, 768);
@@ -17,12 +17,12 @@ function setup() {
   smooth();
   sound.loop();
 
-  for(i = 0; i < nx; i++){
+  for (i = 0; i < nx; i++) {
     a[i] = [];
     b[i] = [];
     c[i] = [];
 
-    for(j = 0; j < ny; j++){
+    for (j = 0; j < ny; j++) {
       a[i][j] = new Dong(65, 150);
       b[i][j] = new Dong(165, 250);
       c[i][j] = new Dong(265, 350);
@@ -32,10 +32,10 @@ function setup() {
 
 function draw() {
   clear();
-  translate(width/2, height/2);
-  rotate(frameCount*0.001);
-  for(i = 0; i < nx; i++){
-    for(j = 0; j < ny; j++){
+  translate(width / 2, height / 2);
+  rotate(frameCount * 0.001);
+  for (i = 0; i < nx; i++) {
+    for (j = 0; j < ny; j++) {
       a[i][j].display();
       b[i][j].display();
       c[i][j].display();
@@ -45,29 +45,28 @@ function draw() {
 
 function Dong(circleSizeMin, circleSizeMax) {
   var isPicked = false,
-      f = random(-Math.PI, Math.PI),
-      s0 = random(2, 6),
-      s1 = 0,
-      x,
-      y;
+    f = random(-Math.PI, Math.PI),
+    s0 = random(2, 6),
+    s1 = 0,
+    x,
+    y;
 
-  x = cos(f)*random(circleSizeMin, circleSizeMax);
-  y = sin(f)*random(circleSizeMin, circleSizeMax);
+  x = cos(f) * random(circleSizeMin, circleSizeMax);
+  y = sin(f) * random(circleSizeMin, circleSizeMax);
 
   this.display = function display() {
-    if(!isPicked){
+    if (!isPicked) {
       fill(255);
-    }
-    else{
+    } else {
       fill(69, 243, 255, 255);
     }
-    s1 += (s0-s1)*0.1;
+    s1 += (s0 - s1) * 0.1;
     ellipse(x, y, s1, s1);
   };
 
 
   this.update = function update(size) {
-    if (isPicked = true){
+    if (isPicked = true) {
       s1 = size;
     }
   };
